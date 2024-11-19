9ed8195dd5c5a217a841281461a05a6ed4d90faa
Landon <l.rodden52@gmail.com>
2024-11-19 13:13:25 -0700

    fixed args issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,17 @@
 # run test-site 
 if [[ $2 == *"test"* ]]
     then
-        cursion test-site $3 --api-token=$1
+        cursion test-site $3 --api-key=$1
 fi
 
 # run run-case 
 if [[ $2 == *"case"* ]]
     then
-        cursion run-case $3 $4 --api-token=$1 $6
+        cursion run-case $3 $4 --api-key=$1 $6
 fi
 
 # run run-flow 
 if [[ $2 == *"flow"* ]]
     then
-        cursion run-flow $3 $5 --api-token=$1
+        cursion run-flow $3 $5 --api-key=$1
 fi
